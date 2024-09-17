71f8bb8b3a71b980f8e6a8b7e5f0e4b0bbf33acb
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2024-09-17 17:39:20 +0200

    tasks

---
+++ src/app/features/courses/courses-list/courses-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+
+@Component({
+  selector: "app-courses-list",
+  templateUrl: "./courses-list.component.html",
+  styleUrls: ["./courses-list.component.css"],
+})
+export class CoursesListComponent {
+  @Input() courses: any[] = [];
+  @Input() editable: string = "";
+
+  @Output() showCourse = new EventEmitter<string>();
+  @Output() editCourse = new EventEmitter<string>();
+  @Output() deleteCourse = new EventEmitter<string>();
+}
