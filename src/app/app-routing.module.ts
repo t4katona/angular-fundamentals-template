0fd5521eebec4fe3066db5000c805f5b85721e8c
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2024-09-30 19:35:29 +0200

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,21 @@
-import { Routes } from '@angular/router';
+import { Routes, provideRouter } from "@angular/router";
+import {
+  CourseCardComponent,
+  CourseFormComponent,
+  LoginFormComponent,
+  RegistrationFormComponent,
+} from "./shared/components";
+import { CoursesComponent } from "./features/courses/courses.component";
+import { ApplicationConfig } from "@angular/platform-browser";
 
 export const routes: Routes = [
-    /* Add your code here */
+  /* Add your code here */
+  { path: "login", component: LoginFormComponent },
+  { path: "registration", component: RegistrationFormComponent },
+  //default
+  { path: "courses", component: CoursesComponent },
+  { path: "**", redirectTo: "courses", pathMatch: "full" },
+  { path: "courses/add", component: CourseFormComponent },
+  { path: "courses/:id", component: CourseCardComponent },
+  { path: "courses/edit/:id", component: CourseFormComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { SharedModule } from "@shared/shared.module";
 import { AppComponent } from "@app/app.component";
-import { CourseInfoComponent } from "@features/course-info/course-info.component";
 import { NotAuthorizedGuard } from "@app/auth/guards/not-authorized.guard";
 import { AuthorizedGuard } from "@app/auth/guards/authorized.guard";
 import { CoursesStoreService } from "@app/services/courses-store.service";
 import { CoursesService } from "@app/services/courses.service";
-import { CoursesComponent } from "./features/courses/courses.component";
-import { CoursesListComponent } from "./features/courses/courses-list/courses-list.component";
 import { CourseInfoModule } from "./features/course-info/course-info.module";
 import { CoursesModule } from "./features/courses/courses.module";
-
+import { RouterModule } from "@angular/router";
+import { routes } from "./app-routing.module";
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -21,6 +19,7 @@ import { CoursesModule } from "./features/courses/courses.module";
     FontAwesomeModule,
     CourseInfoModule,
     CoursesModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [
     AuthorizedGuard,
