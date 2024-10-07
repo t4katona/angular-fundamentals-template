62f8fa2df86f2267240be3469200d282dd34d6fc
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2024-10-07 18:40:00 +0200

    fixen some other tests

--- src/app/features/courses/courses-list/courses-list.component.spec.ts
+++ src/app/features/courses/courses-list/courses-list.component.spec.ts
@@ -1,23 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { CoursesListComponent } from './courses-list.component';
+import { CoursesListComponent } from "./courses-list.component";
+import { HttpClientModule } from "@angular/common/http";
+import { StoreModule } from "@ngrx/store";
 
-describe('CoursesListComponent', () => {
+describe("CoursesListComponent", () => {
   let component: CoursesListComponent;
   let fixture: ComponentFixture<CoursesListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CoursesListComponent ]
-    })
-    .compileComponents();
+      declarations: [CoursesListComponent],
+      imports: [HttpClientModule, StoreModule.forRoot({})],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CoursesListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
