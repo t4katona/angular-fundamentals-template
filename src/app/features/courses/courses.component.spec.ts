11daa8d0e8e5c194e1c8d746f179884aa94a2bed
Katona Tamás <katonatamas@Katona-MacBook-Air.local>
2024-10-07 18:32:41 +0200

    coursesList error fixing

--- src/app/features/courses/courses.component.spec.ts
+++ src/app/features/courses/courses.component.spec.ts
@@ -1,23 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { HttpClientModule } from "@angular/common/http";
+import { StoreModule } from "@ngrx/store";
+import { CoursesComponent } from "./courses.component";
 
-import { CoursesComponent } from './courses.component';
-
-describe('CoursesComponent', () => {
+describe("CoursesComponent", () => {
   let component: CoursesComponent;
   let fixture: ComponentFixture<CoursesComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CoursesComponent ]
-    })
-    .compileComponents();
+      declarations: [CoursesComponent],
+      imports: [HttpClientModule, StoreModule.forRoot({})],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CoursesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
